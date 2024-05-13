282a564a79005130be20e2b1086d779d37f25fb1
Eduardo Leal <eduardo.leeal@hotmail.com>
2024-05-13 17:27:01 -0300

    Commit #2

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,38 @@
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private double saldo;
+    private String titular;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+}
--- ContaBanco/src/Main.java
+++ ContaBanco/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        ContaTerminal conta = new ContaTerminal();
+        System.out.println("Por favor, digite o número da Conta: ");
+        conta.setNumero(sc.nextInt());
+        sc.nextLine();
+        System.out.println("Por favor, digite o número da Agência: ");
+        conta.setAgencia(sc.nextLine());
+        System.out.println("Por favor, digite o nome do titular: ");
+        conta.setTitular(sc.nextLine());
+        System.out.println("Por favor, digite o saldo: ");
+        conta.setSaldo(sc.nextDouble());
+
+        System.out.println("Olá " + conta.getTitular() + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.getAgencia() + ", conta " + conta.getNumero() + " e seu saldo " + conta.getSaldo() + " já esta disponível para saque.");
     }
 }
